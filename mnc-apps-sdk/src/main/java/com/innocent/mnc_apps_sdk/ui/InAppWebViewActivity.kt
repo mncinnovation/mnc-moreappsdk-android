5fe2b31f48951bbb052436382b43ab2c4d28b174
Istiq Septiana <istiq.septiana@mncgroup.com>
2021-02-23 13:45:23 +0700

    Create baseactivity

--- mnc-apps-sdk/src/main/java/com/innocent/mnc_apps_sdk/ui/InAppWebViewActivity.kt
+++ mnc-apps-sdk/src/main/java/com/innocent/mnc_apps_sdk/ui/InAppWebViewActivity.kt
@@ -6,21 +6,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
 import com.innocent.mnc_apps_sdk.R
+import com.innocent.mnc_apps_sdk.base.BaseActivity
 import com.innocent.mnc_apps_sdk.constant.Constant
 import kotlinx.android.synthetic.main.activity_webview.*
-import kotlinx.android.synthetic.main.view_toolbar.*
 
-class InAppWebViewActivity: AppCompatActivity() {
+class InAppWebViewActivity: BaseActivity() {
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_webview)
 
         setLogo()
-        setupToolbar()
+        setDisplayHomeAsUpEnabled(true)
 
         inAppWebView.settings.javaScriptEnabled = true
         inAppWebView.webViewClient = object : WebViewClient() {
@@ -30,20 +28,6 @@ class InAppWebViewActivity: AppCompatActivity() {
             }
         }
         inAppWebView.loadUrl(getWebUrl())
-    }
-
-    private fun setLogo() {
-        Glide.with(this)
-            .asBitmap()
-            .load(Constant.mncLogo)
-            .into(logoImageView)
-    }
-
-    private fun setupToolbar(){
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setDisplayShowHomeEnabled(true)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- mnc-apps-sdk/src/main/java/com/innocent/mnc_apps_sdk/ui/MNCAppsActivity.kt
+++ mnc-apps-sdk/src/main/java/com/innocent/mnc_apps_sdk/ui/MNCAppsActivity.kt
@@ -3,16 +3,14 @@ package com.innocent.mnc_apps_sdk.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.bumptech.glide.Glide
 import com.innocent.mnc_apps_sdk.R
+import com.innocent.mnc_apps_sdk.base.BaseActivity
 import com.innocent.mnc_apps_sdk.constant.Constant
 import com.innocent.mnc_apps_sdk.presenter.MNCAppsPresenter
 import kotlinx.android.synthetic.main.activity_mnc_apps_body.*
-import kotlinx.android.synthetic.main.view_toolbar.*
 
-class MNCAppsActivity : AppCompatActivity(), MNCAppsContract.View {
+class MNCAppsActivity : BaseActivity(), MNCAppsContract.View {
     private val presenter: MNCAppsContract.Presenter = initPresenter()
     private var listAppsAdapter: MNCAppsAdapter? = null
 
@@ -21,24 +19,10 @@ class MNCAppsActivity : AppCompatActivity(), MNCAppsContract.View {
         setContentView(R.layout.activity_mnc_apps_screen)
 
         setLogo()
-        setupToolbar()
+        setDisplayHomeAsUpEnabled(true)
 
         presenter.bind(this)
         presenter.start()
-    }
-
-    private fun setLogo() {
-        Glide.with(this)
-            .asBitmap()
-            .load(Constant.mncLogo)
-            .into(logoImageView)
-    }
-
-    private fun setupToolbar(){
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setDisplayShowHomeEnabled(true)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 
     override fun onSupportNavigateUp(): Boolean {
